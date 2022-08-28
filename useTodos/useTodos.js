d6267d31c16e6c975997ed2771f81441f76ce24c
Álvaro Santos Breval <73903192+asantosb@users.noreply.github.com>
2022-08-28 13:04:42 +0200

    Add files via upload

---
+++ useTodos/useTodos.js
@@ -0,0 +1,47 @@
+import { useReducer, useEffect } from 'react';
+import { todoReducer } from './todoReducer';
+
+
+const init = () => {
+    return JSON.parse(localStorage.getItem("todos")) || [];
+}
+
+export const useTodos = () => {
+  
+    const [todos, dispatch] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    }, [todos]);
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: "ADD_TODO",
+            payload: todo,
+        }
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: "DELETE_TODO",
+            payload: id,
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: "TOGGLE_TODO",
+            payload: id,
+        });
+    }
+
+    return {
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+}
